9eaa520c3c216e449c4b4142e1cb4489482885c4
Katie Emerson <kbemerson27@gmail.com>
2019-10-22 09:44:54 -0600

    Working Game

--- gosu_game.rb
+++ gosu_game.rb
@@ -4,7 +4,7 @@ class Tutorial < Gosu::Window
     def initialize
         super 640, 480
         self.caption = "Tutorial Game"
-        @background_image = Gosu::Image.new("media/space.png", :tileable true)
+        @background_image = Gosu::Image.new("media/space.png", :tileable => true)
         
         @player = Player.new
         @player.warp(320, 240)
@@ -19,28 +19,25 @@ class Tutorial < Gosu::Window
         if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
             @player.turn_left
         end
-        if Gosu.button_down? Gosu:KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
             @player.turn_right
         end
-        def Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+        if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
             @player.accelerate
         end
         @player.move
-        @player.collect_stars(@stars)
+    @player.collect_stars(@stars)
 
-        if rand(100) < 4 and @stars.size < 25
-            @stars.push(Star.new(@star_anim))
-        end
+    if rand(100) < 4 and @stars.size < 25
+      @stars.push(Star.new(@star_anim))
     end
+end
 
-    def draw
-        @background_image.draw(0, 0, ZOrder::BACKGROUND)
-        @player.draw
-        
-        @stars.each { |star| star.draw }
-
-        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-    end
+def draw
+    @background_image.draw(0, 0, ZOrder::BACKGROUND)
+    @player.draw
+    @stars.each { |star| star.draw }
+end
 
     def button_down(id)
         if id == Gosu::KB_ESCAPE
@@ -52,42 +49,39 @@ class Tutorial < Gosu::Window
 end
 
 
-Tutorial.new.show
-
-
 class Player
 attr_reader :score
 
     def initialize
-        @imafe = Gosu:Image.new("media/starfighter.bmp")
+        @image = Gosu::Image.new("media/starfighter.bmp")
         @beep = Gosu::Sample.new("media/beep.wav")
-        @x = @y = @vel_y = @angle = 0.0
+        @x = @y = @vel_x = @vel_y = @angle = 0.0
         @score = 0
     end
 
     def warp(x, y)
         @x, @y = x, y
     end
-
+      
     def turn_left
         @angle -= 4.5
     end
-
+      
     def turn_right
         @angle += 4.5
     end
-
+      
     def accelerate
-        @vel_x += Gosu.offset_x(@anfle, 0.5)
-        @vel_y += Gosu.offset_y(@anfle, 0.5)
+        @vel_x += Gosu.offset_x(@angle, 0.5)
+        @vel_y += Gosu.offset_y(@angle, 0.5)
     end
-
+     
     def move
         @x += @vel_x
         @y += @vel_y
         @x %= 640
         @y %= 480
-
+        
         @vel_x *= 0.95
         @vel_y *= 0.95
     end
@@ -102,7 +96,7 @@ attr_reader :score
 
     def collect_stars(stars)
         stars.reject! do |star| 
-            if Gosu.distance(@x, @y, star.x, star.y) < 35 }
+            if Gosu.distance(@x, @y, star.x, star.y) < 35
                 @score += 10
                 @beep.play
                 true
@@ -132,9 +126,11 @@ class Star
         @y = rand * 480
     end
 
-    def draw
-        img = @animation[Gosu.milliseconds / 100 % animation.size]
+    def draw  
+        img = @animation[Gosu.milliseconds / 100 % @animation.size]
         img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
             ZOrder::STARS, 1, 1, @color, :add)
-    end
+      end
 end
+
+Tutorial.new.show
